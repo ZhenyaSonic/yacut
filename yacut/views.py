b7fe0617656462991ed36ce3e2ba2fbac673cc2a
Евгений Братанов <sonik.zhenyaa@gmail.com>
2025-01-19 11:31:28 +0300

    Готовый сервис2

--- yacut/views.py
+++ yacut/views.py
@@ -12,10 +12,17 @@ from .models import URLMap
 
 def generate_random_link(length=6):
     characters = string.ascii_letters + string.digits
-    random_link = ''.join(random.choice(characters) for _ in range(length))
-    if check_duplicate(random_link):
-        random_link = generate_random_link()
-    return random_link
+    while True:
+        random_link = ''.join(random.choice(characters) for _ in range(length))
+        if not check_duplicate(random_link):
+            return random_link
+
+
+def is_valid_custom_id(custom_id):
+    return (
+        len(custom_id) <= MAX_LENGTH_SHORT and
+        re.match('^[a-zA-Z0-9]+$', custom_id)
+    )
 
 
 def check_duplicate(short):
@@ -25,36 +32,27 @@ def check_duplicate(short):
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = UrlForm()
-    if not form.validate_on_submit():
-        return render_template('create_short.html', form=form)
-    if form.custom_id.data != '' and form.custom_id.data is not None:
-        short = form.custom_id.data
-        if (
-                len(short) > MAX_LENGTH_SHORT
-                or not re.findall('^[a-zA-Z0-9]+$', short)
-        ):
-            flash('Указано недопустимое имя для короткой ссылки')
-            return render_template('create_short.html', form=form)
-        if check_duplicate(short):
-            flash('Предложенный вариант короткой ссылки уже существует.')
-            return render_template('create_short.html', form=form)
-    else:
-        short = generate_random_link()
-    url_map = URLMap(
-        original=form.original_link.data,
-        short=short
-    )
-    db.session.add(url_map)
-    db.session.commit()
-    return render_template(
-        'create_short.html',
-        form=form,
-        link=url_for(
-            'redirect_view',
-            short=url_map.short,
-            _external=True
+    if form.validate_on_submit():
+        if form.custom_id.data:
+            short = form.custom_id.data
+            if not is_valid_custom_id(short):
+                flash('Указано недопустимое имя для короткой ссылки')
+                return render_template('create_short.html', form=form)
+            if check_duplicate(short):
+                flash('Предложенный вариант короткой ссылки уже существует.')
+                return render_template('create_short.html', form=form)
+        else:
+            short = generate_random_link()
+
+        url_map = URLMap(original=form.original_link.data, short=short)
+        db.session.add(url_map)
+        db.session.commit()
+        return render_template(
+            'create_short.html',
+            form=form,
+            link=url_for('redirect_view', short=url_map.short, _external=True)
         )
-    )
+    return render_template('create_short.html', form=form)
 
 
 @app.route('/<short>')
